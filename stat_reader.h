15d02bc85f7025b661010730c6978c5e8bae8b14
Nikita <66407002+Babloved@users.noreply.github.com>
2024-12-13 15:51:53 +1000

    Add base functional

---
+++ stat_reader.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iosfwd>
+#include <string_view>
+#include "transport_catalogue.h"
+
+namespace statreader{
+    struct CommandDescription {
+        // Определяет, задана ли команда (поле command непустое)
+        explicit operator bool() const {
+            return !command.empty();
+        }
+
+        bool operator!() const {
+            return !operator bool();
+        }
+
+        std::string command;      // Название команды
+        std::string id;           // id маршрута или остановки
+        std::string description;  // Параметры команды
+    };
+    void ParseAndPrintStat(const tc::TransportCatalogue& tansport_catalogue, std::string_view request,
+                    std::ostream& output);
+}
+
